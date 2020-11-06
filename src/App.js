f889348e46b0c31927a9b4a552dadfa0df727419
Alan Di Masi <alan199912@gmail.com>
2020-11-06 17:40:03 -0300

    [status]: Upload Front

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Link,  Switch, Redirect  } from 'react-router-dom'
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Components
+import Nav from './components/Nav/nav';
+import Detail from './components/Detail/detail';
+import Home from './components/Home/home';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Router>            
+          <Route exact path="/" render={() => {
+            return (
+              <div>
+                <Nav/>
+              </div>
+            )
+            }}>
+          </Route>
+      </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
